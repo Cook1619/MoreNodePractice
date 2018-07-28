52f9b61bdd8fc67f48a3247a30376115bffcd698
Cook1619 <cook6348@gmail.com>
2018-07-28 14:19:04 -0500

    Able to pull title data from another web request

--- server/server2.js
+++ server/server2.js
@@ -1,13 +1,25 @@
 let path = require('path');
 let fs = require('fs');
+const request = require('request');
 
-let dataPath = path.join(__dirname, "../moreData.json");
 
-fs.readFile(dataPath, {
-    encoding: "UTF-8"
-}, (err, information) => {
-    let packersInfo = JSON.parse(information);
-    console.log(packersInfo.team);
-    console.log(packersInfo.location);
-    console.log(packersInfo.championships);
-})
+let dataPath = path.join(__dirname, "../jsonPlaceholder.json");
+
+request('https://jsonplaceholder.typicode.com/posts', (err, res, body) => {
+
+    if(err) console.log(err);
+
+    JSON.parse(body).forEach((title)=>{
+        fs.appendFileSync(dataPath, title.title + '\n');
+    });
+});
+
+
+// fs.readFile(dataPath, {
+//     encoding: "UTF-8"
+// }, (err, information) => {
+//     let packersInfo = JSON.parse(information);
+//     console.log(packersInfo.team);
+//     console.log(packersInfo.location);
+//     console.log(packersInfo.championships);
+// })
