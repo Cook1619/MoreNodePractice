26a8d357645249bbd5b0bde21fc080ec2454aee9
Cook1619 <cook6348@gmail.com>
2018-07-28 13:56:29 -0500

    Writing out some data from reddit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,22 @@
-let path = require('path');
-let fs = require('fs');
+const path = require('path');
+const fs = require('fs');
+const request = require('request');
+
 
 let dataPath = path.join(__dirname, '../data.json');
 
-fs.readFile(dataPath, {
-    encoding: "UTF-8"
-}, (err, data) =>{
-    let person = JSON.parse(data);
-    console.log(person.name);
-    console.log(person.shirtColor);
-})
+request('https://reddit.com/r/popular.json', (err, res, body) => {
+
+    if (err) console.log(err);
+
+    JSON.parse(body).data.children.forEach((item) => {
+        fs.appendFileSync(dataPath, item.data.title + '\n');
+    });
+});
+// fs.readFile(dataPath, {
+//     encoding: "UTF-8"Yesi
+// }, (err, data) =>{
+//     let person = JSON.parse(data);
+//     console.log(person.name);
+//     console.log(person.shirtColor);
+// })
