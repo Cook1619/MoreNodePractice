0632e3fe6e81831bdfd2c4be89945555d2ba399b
Cook1619 <cook6348@gmail.com>
2018-07-28 12:38:11 -0500

    Understanding more how files get read and data parsed

--- server/server3.js
+++ server/server3.js
@@ -1,4 +1,21 @@
 let path =  require('path');
 let fs = require('fs');
 
-let findData = 
+let findData = path.join(__dirname, "../data.json");
+let packerData = path.join(__dirname, "../moreData.json");
+
+fs.readFile(findData, {
+    encoding: "UTF-8"
+},(err,data) => {
+    let moreData = JSON.parse(data);
+    console.log(moreData.name);
+    console.log(moreData.shirtColor);
+})
+
+fs.readFile(packerData, {
+    encoding:"UTF-8"
+}, (err, data2) => {
+   let packers = JSON.parse(data2);
+   console.log(packers.team);
+   console.log(packers.location);
+})
